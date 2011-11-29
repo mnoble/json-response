3ce3367b4ac443ccf24f922301b11392739dd690
Matte Noble <me@mattenoble.com>
2011-11-29 14:15:01 -0500

    Lazy evaluate define blocks to support increment counters

--- lib/json_response.rb
+++ lib/json_response.rb
@@ -5,16 +5,39 @@ module JSONResponse
   VERSION = "0.0.1"
   extend self
 
-  def define(name)
-    yield (registry[name] = OpenStruct.new)
+  def counters
+    @counters ||= Hash.new(0)
+  end
+
+  def next(name)
+    counters[name] += 1
+  end
+
+  class JSONStruct < OpenStruct
+    def initialize(&block)
+      super
+      @block = block
+    end
+
+    def next(name)
+      JSONResponse.next(name)
+    end
+
+    def build
+      self.tap { |s| @block.call(s) }.marshal_dump
+    end
+  end
+
+  def define(name, &block)
+    registry[name] = JSONStruct.new(&block)
   end
 
   def build(name, options={})
-    JSON.generate(registry[name].marshal_dump.merge(options))
+    JSON.generate(registry[name].build.merge(options))
   end
 
   def attributes_for(name, options={})
-    registry[name].marshal_dump.merge(options)
+    registry[name].build.merge(options)
   end
 
 private
--- spec/json_response_spec.rb
+++ spec/json_response_spec.rb
@@ -2,14 +2,20 @@ require File.dirname(__FILE__) + '/spec_helper'
 
 describe JSONResponse, ".build" do
   before do
-    JSONResponse.define(:person) { |p| p.name = "Dave" }
+    JSONResponse.define(:person) { |p| p.id = JSONResponse.next(:id); p.name = "Dave" }
   end
 
   it "builds json strings from definitions" do
-    JSONResponse.build(:person).should == "{\"name\":\"Dave\"}"
+    JSONResponse.build(:person).should == '{"id":1,"name":"Dave"}'
   end
 
   it "builds a hash of attributes from definitions" do
-    JSONResponse.attributes_for(:person).should == {name: "Dave"}
+    JSONResponse.attributes_for(:person).should == {id: 2, name: "Dave"}
+  end
+
+  it "supports counters" do
+    count = JSONResponse.counters[:id]
+    JSONResponse.attributes_for(:person)[:id].should == count + 1
+    JSONResponse.attributes_for(:person)[:id].should == count + 2
   end
 end
